c293153ea57f86a3193022ec61c5931c7809b293
cmacdonald <cmac1000_user@server.fake>
2013-09-16 10:22:25 -0400

    release branch for combinator 2.0

--- combinator.py
+++ combinator.py
@@ -1,159 +1,210 @@
-from pyPdf import PdfFileWriter, PdfFileReader
-import pyPdf
+# -*- coding: utf-8 -*-
+
+from pyPdf import PdfFileReader, PdfFileWriter
+import re
 import os
 
-try:
-    os.mkdir "output/"
-except:
-    print "did not create output dir"
+# Classes
 
+class Configuration(object):
+    """
+    Hold logic for assembling documents and directing output
+    """
 
-class Pdf(object):
-    '''
-    Object to associate the various documents of a given application
-    Perhaps I should rename this.
-    '''
-    def __init__(self, refCode):
-        self.refCode = refCode
-        self.appPdf = None
-        self.recPdfs = []
-        self.uploads = []
+    def __init__(self, config_directory = "./config"):
+        """
+        _input_directory: Directory of PDFs to handle
+        _documents_to_ignore: information about documents to ignore
+        _recommendation_application_associations: rec -> app association dict
+        _application_filenames: app -> filename dict
+        _document_order: List of strings to be found in filenames, in order for output
+        """
+        self._config_directory = config_directory
+        self._input_directory = "./input"
+        self._documents_to_ignore = self._load_documents_to_ignore()
+        self._recommendation_application_associations = self._load_recommendation_application_associations()
+        self._application_filenames = self._load_application_filenames()
+        self._document_order = self._load_document_order()
 
-    def addAppPdf(self,filename):
-        self.appPdf = filename
-    def addRecPdf(self,filename):
-        self.recPdfs.append(filename)
-    def addUpload(self,filename):
-        self.uploads.append(filename)
-    def getRefCode(self):
-        return self.refCode
-    def getAppPdf(self):
-        return self.appPdf
-    def getRecPdfs(self):
-        return self.recPdfs
-    def getUploads(self):
-        return self.uploads
-        
-def getFiles():
-    '''
-    get all files in directory
-    '''
-    return [file for file in os.listdir('.')]
+    def _load_documents_to_ignore(self):
+        """
+        Looks for this file: documents_to_ignore.txt
+        if it is found, loads documents_to_ignore based on that file
+        if it's not found, returns a sensible default
+        """
+        config_document = "".join([self._config_directory, "/documents_to_ignore.txt"])
+        output_list = parse_config_file_into_list(config_document)
+        return output_list
 
-def printFiles():
-    '''
-    print all files in directory
-    '''
-    for file in getFiles():
-        print file
+    def _load_recommendation_application_associations(self):
+        """
+        Looks for this file: recommendation_application_associations.txt
+        if it is found, loads recommendation_application_associations based on that file
+        if it's not found, returns a sensible default
+        """
+        config_document = "".join([self._config_directory, "/recommendation_application_associations.txt"])
+        output_dict = parse_config_file_into_dictionary(config_document)
+        return output_dict
 
-def getRefCode(filename):
-    '''
-    Extracts the first 8 characters of a filename, on the assumption that they are a reference code
-    '''
-    return str(filename[:8])
+    def _load_application_filenames(self):
+        """
+        Looks for this file: application_filenames.txt
+        if it is found, loads application_filenames based on that file
+        if it's not found, returns a sensible default
+        """
+        config_document = "".join([self._config_directory, "/application_filenames.txt"])
+        output_list = parse_config_file_into_dictionary(config_document)
+        return output_list
 
-def getUniqueRefCodes():
-    '''
-    Gets Unique Reference Codes for documents in given directory
-    '''
-    refCodes = []
-    for file in getFiles():
-        if '.pdf' not in file:
-            continue
-        elif getRefCode(file) not in refCodes:
-            refCodes.append(getRefCode(file))
-    return refCodes
+    def _load_document_order(self):
+        """
+        Looks for this file: document_order.txt
+        if it is found, loads document_order based on that file
+        if it's not found, returns a sensible default
+        """
+        config_document = "".join([self._config_directory, "/document_order.txt"])
+        output_list = parse_config_file_into_list(config_document)
+        return output_list
 
-def createPDFObjects():
-    '''
-    Creates pdf objects
-    '''
-    Pdfs = []
-    for refCode in getUniqueRefCodes():
-        Pdfs.append(Pdf(refCode))
-    return Pdfs
+    def get_input_directory(self):
+        if self._input_directory is not None:
+            return self._input_directory
 
-def findPdfObject(refCode, pdfList):
-    '''
-    refCode: string
-    pdfList: list of Pdf objects
+    def get_documents_to_ignore(self):
+        if self._documents_to_ignore is not []:
+            return self._documents_to_ignore
 
-    if a matching reference code is found in pdfList, returns that pdf object
-    otherwise, returns None
-    '''
-    for pdf in pdfList:
-        if pdf.getRefCode() == refCode:
-            return pdf
-    return None
-    
-def combine(pdfList, files = getFiles()):
-    '''
-    Combines files found in a folder into Pdf objects, based on strings found in their filenames
-    '''
-    for file in files:
-##        if ".pdf" not in file:
-##            break
-##        if 'App' in file:
-##            print file, 'app'
-##        elif 'Teacher' in file:
-##            print file, 'teacher'
-##        elif 'Parent' in file:
-##            print file, 'parent'
-##        else:
-##            print "PROBLEM", file
-        refCode = getRefCode(file)
-        pdf = findPdfObject(refCode,pdfList)
-        if 'App' in file:
-            pdf.addAppPdf(file)
-        elif 'rec' in file:
-            pdf.addRecPdf(file)
+    def get_recommendation_application_associations(self):
+        if self._recommendation_application_associations is not {}:
+            return self._recommendation_application_associations
+
+    def get_application_filenames(self):
+        if self._application_filenames is not {}:
+            return self._application_filenames
+
+    def get_document_order(self):
+        if self._document_order is not []:
+            return self._document_order
+
+class Document(object):
+    """
+    Represents final or output document, contains some number of filenames. Initialize with an applicant reference code and an output filename
+    """
+
+    def __init__(self,applicant_reference_code, output_filename):
+        """
+        initialize internal variables:
+
+        _filenames: list of filenames to be combined
+        _applicant_reference_code: string; the applicant's reference code
+        _output_filename: the filename that should be used for the final output
+        """
+        self._filenames = []
+        self._applicant_reference_code = applicant_reference_code
+        self._output_filename = output_filename
+
+    def contains_encrypted_pdfs(self):
+        """
+        Returns True if there are encrypted PDFs in self._filenames, False otherwise
+        """
+        for filename in self._filenames:
+            if is_encrypted(filename) == True:
+                return True
         else:
-            if ".pdf" in file or ".PDF" in file:
-                pdf.addUpload(file)
+            return False
 
+    def find_encrypted_pdfs(self):
+        """
+        Returns list of all encrypted PDFs in Document, empty list if there are none
+        """
+        output_list = []
+        for filename in self._filenames:
+            if is_encrypted(filename) == True:
+                output_list.append(filename)
+        return output_list
 
-        
-def getPdfs():
-    '''
-    returns all pdf files found in a given directory
-    '''
-    Pdf = []
-    for file in getFiles():
-        if '.pdf' in file:
-            Pdf.append(file)
-    return Pdf
+    def sort_filenames(self, configuration):
+        """
+        Puts self._filenames into the correct order for printing, based on logic in configuration_object
+        """
+        nested_list = []
+        filenames_to_sort = self._filenames[:]
+        document_order = configuration.get_document_order()
+        for pattern in document_order:
+            tier_list = find_matching_filenames(filenames_to_sort, pattern)
+            for filename in tier_list:
+                filenames_to_sort.remove(filename)
+            if tier_list != []:
+                nested_list.append(tier_list)
+        if filenames_to_sort:
+            filenames_to_sort.sort()
+            nested_list.append(filenames_to_sort)
+        flattened_list = flatten_list(nested_list)
+        if len(flattened_list) != len(self._filenames):
+            raise Exception
+        self._filenames = flattened_list
+        return True
 
+    def add_filename(self, filename):
+        self._filenames.append(filename)
+        return True
 
-def makeOutputPdf(pdf):
-    '''
-    Takes a Pdf object, combines all its constituent parts into a pdf document in /output directory
-    '''
-    output = PdfFileWriter()
-    pieces = []
-    if pdf.getAppPdf() != None:
-        pieces.append(pdf.getAppPdf())
-    for rec in pdf.getRecPdfs():
-        pieces.append(rec)
-    for rec in pdf.getUploads():
-        pieces.append(rec)
-    for piece in pieces:
-        if isPdfEncrypted(piece) == True:
-            raise Exception(piece)
-    for piece in pieces:
-        if isPdfEncrypted(piece) == False:
-            input = PdfFileReader(file(piece, "rb"))
-            numPages = input.getNumPages()
+    def get_filenames(self):
+        if self._filenames is not []:
+            # self.sort_filenames()
+            return self._filenames
+        else:
+            raise NoFilenamesException
+
+    def get_applicant_reference_code(self):
+        if self._applicant_reference_code is not None:
+            return self._applicant_reference_code
+        else:
+            raise UnknownReferenceCodeException
+
+    def get_output_filename(self):
+        if self._output_filename is not None:
+            return self._output_filename
+        else:
+            raise UnknownOutputFilenameException
+
+class Combinator(object):
+    """
+    Uses the combinate() method on Document objects to produce PDFs
+    """
+
+    def __init__(self):
+        """
+        initialize internal variables:
+
+        _output_directory: string; path to directory for output
+        """
+        self._output_directory = "./output"
+
+    def combine(self, document):
+        """
+        Takes document object, combines the constituent filenames and outputs to self._output_directory
+        """
+        output = PdfFileWriter()
+        filenames = document.get_filenames()
+        for filename in filenames:
+            input_file = PdfFileReader(file(filename, "rb"))
+            numPages = input_file.getNumPages()
             for page in range (numPages):
-                output.addPage(input.getPage(page))
-        else:
-            print "skipping", piece
-        outputName = "output/" + pdf.getRefCode() + "-Full.pdf"
-        outputStream = file(outputName, "wb")
-        output.write(outputStream)
-        outputStream.close()
+                output.addPage(input_file.getPage(page))
+        output_filename = "".join([self._output_directory, "/", document.get_output_filename()])
+        output_stream = file(output_filename, "wb")
+        output.write(output_stream)
+        output_stream.close()
+        return True
+    
+    def set_output_directory(self, output_directory):
+        self._output_directory = output_directory
+        return True
 
-def isPdfEncrypted(filename):
+# Functions
+
+def is_encrypted(filename):
     '''
     First-pass attempt to see if a given pdf will give the makeOutputPdf function trouble.
     Returns True if PDFFileReader object gives an exception when trying to get the number of pages
@@ -162,73 +213,145 @@ def isPdfEncrypted(filename):
     try:
         numPages = input.getNumPages()
     except:
-        #print filename
         return True
     return False
-        
-def listProblemPdfs():
+
+def create_document_objects(configuration):
+    """
+    based on configuration object, initializes document objects, returns as a list
+    """
+    output_list = []
+    filename_dict = configuration.get_application_filenames()
+    for input_filename in filename_dict.keys():
+        output_filename = filename_dict[input_filename]
+        applicant_reference_code = extractReferenceCode(input_filename)
+        document = Document(applicant_reference_code,output_filename)
+        output_list.append(document)
+    return output_list
+
+def find_document(filename, documents, configuration):
+    """
+    given a filename as a string, figures out which document object it should be associated with, returns that document object
+    """
+    document_reference_code = extractReferenceCode(filename)
+    target_reference_code = None
+    if document_reference_code in configuration.get_application_filenames().keys():
+        # Belongs to application
+        target_reference_code = document_reference_code
+    elif document_reference_code in configuration.get_recommendation_application_associations().keys():
+        target_reference_code = configuration.get_recommendation_application_associations()[document_reference_code]
+    for document in documents:
+        if target_reference_code == document.get_applicant_reference_code():
+            return document
+
+def find_all_encrypted_pdfs(documents):
+    """
+    given a list of document objects, finds all the encrypted pdfs in each one and returns a list
+    """
+    nested_list = []
+    for document in documents:
+        encrypted_documents = document.find_encrypted_pdfs()
+        if encrypted_documents != []:
+            nested_list.append(encrypted_documents)
+    flattened_list = flatten_list(nested_list)
+    flattened_list.sort()
+    return flattened_list
+
+def parse_config_file_into_dictionary(filename):
+    """
+    assumes tab-delimited file
+    """
+    output_dict = {}
+    input_file = open(filename, "rb")
+    for line in input_file:
+        items = line.split("\t")
+        if len(items) != 2:
+            raise Exception
+        else:
+            output_dict[items[0].strip()] = items[1].strip()
+    return output_dict
+
+def parse_config_file_into_list(filename):
+    output_list = []
+    input_file = open(filename, "rb")
+    for line in input_file:
+        output_list.append(line.strip())
+    return output_list
+
+def extractReferenceCode(filename):
     '''
-    Lists all the Pdfs that will give PdfFileReader object trouble
+    takes filename as string
+    returns reference code as string in format AAAA1111
     '''
-    for pdf in getFiles():
-        try:
-            isPdfEncrypted(pdf)
-        except:
-            print pdf
+    regex = re.compile('[A-Z]{4}[\d]{4}')
+    result = regex.findall(filename)
+    if len(result) != 1:
+        print filename
+        print result
+        print len(result)
+        raise Exception
+    if len(result[0]) != 8:
+        raise Exception
+    return result[0]
 
-def listPdfs(list):
-    for pdf in list:
-        print pdf.getRefCode()
-        print pdf.getAppPdf()
-        print pdf.getRecPdfs()
-        print pdf.getUploads()
-        print "###"
+def find_matching_filenames(filename_list, text_to_match):
+    """
+    find all filenames that match that pattern, returns them as a list
+    """
+    output_list = []
+    for filename in filename_list:
+        if text_to_match in filename:
+            output_list.append(filename)
+    return output_list
 
+def flatten_list(input_list):
+    """
+    flattens a tiered list of lists into one list
+    """
+    # http://stackoverflow.com/questions/952914/making-a-flat-list-out-of-list-of-lists-in-python
+    if type(input_list) != list:
+        raise Exception
+    else:
+        return [item for sublist in input_list for item in sublist]
 
+# Exceptions
 
+class NotImplementedException(Exception):
+    def __init__(self):
+        pass
 
-def go():
-    for pdf in pdfList:
-        try:
-            makeOutputPdf(pdf)
-        except:
-            print pdf.getRefCode(), "not created"
+class NoFilenamesException(Exception):
+    def __init__(self):
+        pass
 
-### Setup
+class UnknownReferenceCodeException(Exception):
+    def __init__(self):
+        pass
 
-pdfList = createPDFObjects()
-combine(pdfList)
+class UnknownOutputFilenameException(Exception):
+    def __init__(self):
+        pass
+# Main
 
-
-####
-
-### Nonsense and half-formed ideas graveyard
-
-
-# def outputter(pieces):
-#     print pieces
-#     output = PdfFileWriter()
-#     for piece in pieces:
-#         input = PdfFileReader(file(piece, "rb"))
-#         try:
-#             numPages = input.getNumPages()
-#         except:
-#             print piece
-# ##        for page in range (numPages):
-# ##            output.addPage(input.getPage(page))
-# ##    outputName = "output/" + pieces[1]
-# ##    outputStream = file(outputName, "wb")
-# ##    output.write(outputStream)
-# ##    outputStream.close()
-
-##def makeOutputPdf2():
-##    index = 0
-##    pdfs = getPdfs()
-##    while index < len(pdfs):
-##        pieces = []
-##        pieces.append(pdfs[index])
-##        pieces.append(pdfs[index + 1])
-##        print pieces
-##        print '###'
-##        outputter(pieces)
-##        index += 2
+def main():
+    try:
+        os.mkdir("output")
+    except WindowsError:
+        pass
+    configuration = Configuration()
+    combinator = Combinator()
+    documents = create_document_objects(configuration)
+    for filename in os.listdir(configuration.get_input_directory()):
+        # configuration.get_input_directory()
+        document = find_document(filename, documents, configuration)
+        document.add_filename("".join([configuration.get_input_directory(),"/", filename]))
+    for document in documents:
+        document.sort_filenames(configuration)
+    encrypted_pdfs = find_all_encrypted_pdfs(documents)
+    if len(encrypted_pdfs) == 0:
+        for document in documents:
+            combinator.combine(document)
+    else:
+        print("encrypted pdfs found:")
+        for filename in encrypted_pdfs:
+            print filename
---
+++ combinator_tests.py
@@ -0,0 +1,66 @@
+from combinator import Configuration, Document
+import unittest
+
+class Configuration_Tests(unittest.TestCase):
+    def setUp(self):
+        self.configuration = Configuration("./test_config")
+    def test_get_input_directory(self):
+        assert self.configuration.get_input_directory() == "./input"
+    def test_get_documents_to_ignore(self):
+        assert self.configuration.get_documents_to_ignore() == ["UpADocumentToIgnore",
+        "AndAnotherOne"]
+    def test_get_recommendation_application_associations(self):
+        assert self.configuration.get_recommendation_application_associations() == {"AJDK5462":"SJDK2136",
+        "SLE82654":"SCIE9643",
+        "ALDJ2136":"POEU2146",
+        "CJEU3216":"PSOF3215",
+        "SLDK2643":"AODJ5468"}
+    def test_get_application_filenames(self):
+        assert self.configuration.get_application_filenames() == {"AJDK5462":"Some_Application_John_Smith_AJDK5462",
+        "SLE82654":"Some_Application_Jim_John_SLE82654",
+        "ALDJ2136":"Some_Application_Elon_Musk_ALDJ2136",
+        "CJEU3216":"Some_Application_Old_Man_Winter_CJEU3216",
+        "SLDK2643":"Some_Application_The_Friend_SLDK2643"}
+    def test_get_document_order(self):
+        assert self.configuration.get_document_order() == ["Application",
+        "Up Resume",
+        "UpPassport",
+        "Recommendation"]
+
+class Document_Tests(unittest.TestCase):
+    def setUp(self):
+        self.document = Document("ABCD2356","Some_Application_Neal_Knight_ABCD2356.pdf")
+        self.configuration = Configuration("./test_config")
+    def test_contains_encrypted_pdfs(self):
+        assert self.document.contains_encrypted_pdfs() == False
+    def test_find_encrypted_pdfs(self):
+        assert self.document.find_encrypted_pdfs() == []
+    def test_get_filenames(self):
+        assert self.document.get_filenames() == []
+    def test_add_filename(self):
+        assert self.document.get_filenames() == []
+        self.document.add_filename("here_is_a_file.txt")
+        self.document.add_filename("here's_another_one.pdf")
+        assert len(self.document.get_filenames()) == 2
+        assert "here_is_a_file.txt" in self.document.get_filenames()
+    def test_sort_filenames(self):
+        self.document.add_filename("here_is_a_file.txt")
+        self.document.add_filename("here's_another_one.pdf")
+        self.document.add_filename("Application_45632.friend")
+        self.document.add_filename("some_Recommendation.ext")
+        assert self.document.get_filenames() == ["here_is_a_file.txt",
+        "here's_another_one.pdf",
+        "Application_45632.friend",
+        "some_Recommendation.ext"]
+        self.document.sort_filenames(self.configuration)
+        assert self.document.get_filenames() == ["Application_45632.friend",
+        "some_Recommendation.ext",
+        "here's_another_one.pdf",
+        "here_is_a_file.txt"]
+    def test_get_applicant_reference_code(self):
+        assert self.document.get_applicant_reference_code() == "ABCD2356"
+    def test_get_output_filename(self):
+        assert self.document.get_output_filename() == "Some_Application_Neal_Knight_ABCD2356.pdf"
+
+if __name__ == "__main__":
+    unittest.main()
